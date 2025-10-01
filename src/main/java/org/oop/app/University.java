913b2155469169e8c962e79cfb47018dba260574
Ionut <bmugurel24@gmail.com>
2025-10-01 22:14:27 +0300

    am adăugat getter și setter pentru University

--- src/main/java/org/oop/app/University.java
+++ src/main/java/org/oop/app/University.java
@@ -5,5 +5,34 @@ public class University {
     private String country;
     private int ranking;
 
-    // Constructor, getteri, setteri
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getRanking() {
+        return ranking;
+    }
+
+    public void setRanking(int ranking) {
+        this.ranking = ranking;
+    }
+
+    public University(String name, String country, int ranking) {
+        this.name = name;
+        this.country = country;
+        this.ranking = ranking;
+    }
+// Constructor, getteri, setteri
 }
