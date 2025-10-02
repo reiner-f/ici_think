1b1b743733fc9f6c195f725f2ccad337cf124253
Ionut <bmugurel24@gmail.com>
2025-10-02 10:03:42 +0300

    salvare locală temporară

--- src/main/java/org/oop/app/University.java
+++ src/main/java/org/oop/app/University.java
@@ -4,7 +4,7 @@ public class University {
     private String name;
     private String country;
     private int ranking;
- 
+
 // Constructor, getteri, setteri
 public String getName() {
     return name;
